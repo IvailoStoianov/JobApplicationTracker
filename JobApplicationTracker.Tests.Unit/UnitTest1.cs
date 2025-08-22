107d199d942e7b3dae8a34cad5aebc71466ec4f8
IvailoStoianov <stivailo05@gmail.com>
2025-08-22 07:24:27 +0300

    Add project files.

---
+++ JobApplicationTracker.Tests.Unit/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿namespace JobApplicationTracker.Tests.Unit
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
